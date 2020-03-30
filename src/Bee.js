9116e1a7aa29e9efa6bb2a51188b63738a728d61
Michael Justice <m_a_justice@yahoo.com>
2020-03-30 13:32:34 -0700

    completed grub/bee w/ Paul Ferguson

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-class Bee {
-  // TODO..
-};
+class Bee extends Grub {
+  // set constructor
+  constructor (age, color, job) {
+    // set this
+    super ();
+    this.age = 5;
+    this.color = 'yellow';
+    this.job = 'Keep on growing';
+  }
+}
--- src/Grub.js
+++ src/Grub.js
@@ -1,2 +1,13 @@
 class Grub {
-};
+  // constructor
+  constructor(age, color, food) {
+    // properties\
+    this.age = 0;
+    this.color = 'pink';
+    this.food = 'jelly';
+  }
+  // methods
+  eat() {
+    return `Mmmmmmmmm ${this.food}`;
+  }
+}
